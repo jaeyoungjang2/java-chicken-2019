e08941321fa1240202bc2c53f65217fd04a197a2
jaeyoungjang2 <lion85488@gmail.com>
2021-11-29 10:10:10 +0900

    feat: 메인메뉴 선택화면 출력 및 선택

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,12 +10,19 @@ import java.util.List;
 public class Application {
     // TODO 구현 진행
     public static void main(String[] args) {
+        // 메인 화면 출력
+        int mainNumber = InputView.inputMainNumber();
+
+        // 테이블 번호 출력
         final List<Table> tables = TableRepository.tables();
         OutputView.printTables(tables);
 
         final int tableNumber = InputView.inputTableNumber();
 
+        // 메뉴 번호 출력
         final List<Menu> menus = MenuRepository.menus();
         OutputView.printMenus(menus);
+
+
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,12 @@ public class InputView {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputMainNumber() {
+        System.out.println("## 메인화면");
+        System.out.println("1 - 주문등록");
+        System.out.println("2 - 결제하기");
+        System.out.println("3 - 프로그램 종료");
+        return scanner.nextInt();
+    }
 }
